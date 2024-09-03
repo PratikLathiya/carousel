a18fb793925ddf2a8e998eb7d1c6783f97026cb8
PratikLathiya <pratiklathiya39@gmail.com>
2024-09-03 15:29:10 +0200

    first commit

---
+++ src/components/sliderImage.js
@@ -0,0 +1,41 @@
+// import First from "../assets/First.jpg";
+// import Second from "../assets/Second.jpg";
+// import Third from "../assets/Third.jpg";
+
+export default [
+  {
+    title: "First Slide",
+    description: "This is the first slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(100).jpg",
+  },
+  {
+    title: "Second Slide",
+    description: "This is the second slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(131).jpg",
+  },
+  {
+    title: "Third Slide",
+    description: "This is the Third slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(150).jpg",
+  },
+  {
+    title: "Fourth Slide",
+    description: "This is the Fourth slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(140).jpg",
+  },
+  {
+    title: "Fifth Slide",
+    description: "This is the Fifth slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(148).jpg",
+  },
+  {
+    title: "Sixth Slide",
+    description: "This is the Sixth slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(106).jpg",
+  },
+  {
+    title: "Seventh Slide",
+    description: "This is the Seventh slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(102).jpg",
+  },
+];
